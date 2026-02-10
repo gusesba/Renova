7ee71ef4eb2f1e808c120040951d3e5dc69ccb9c
Gustavo Esmanhotto Bareta <gustavoesmanhotto@hotmail.com>
2026-02-10 10:29:47 -0300

    Alterar autenticação para uso de cookie HttpOnly

--- RenovaBackendDotNet/Renova.API/Controllers/UsuarioController.cs
+++ RenovaBackendDotNet/Renova.API/Controllers/UsuarioController.cs
@@ -1,7 +1,7 @@
-﻿using Renova.Domain.Model.Dto;
-using Renova.Service.Commands;
+﻿using Renova.Service.Commands;
 using Renova.Service.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Renova.API.Controllers
@@ -10,6 +10,7 @@ namespace Renova.API.Controllers
     [Route("api/usuario")]
     public class UsuarioController : ControllerBase
     {
+        private const string AuthCookieName = "renova_auth";
         private readonly IMediator _mediator;
         public UsuarioController(IMediator mediator)
         {
@@ -17,24 +18,59 @@ namespace Renova.API.Controllers
         }
 
         [HttpPost("login")]
-        [ProducesResponseType(typeof(LoginDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public async Task<IActionResult> Login([FromBody] LoginQuery request)
         {
             var token = await _mediator.Send(request);
             if (token == null)
                 return Unauthorized();
 
-            return Ok(token);
+            DefinirCookieAutenticacao(token.Token);
+
+            return NoContent();
         }
 
         [HttpPost("registrar")]
-        [ProducesResponseType(typeof(LoginDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
 
         public async Task<IActionResult> Registrar([FromBody] SignUpCommand command)
         {
             var token = await _mediator.Send(command);
 
-            return Ok(token);
+            DefinirCookieAutenticacao(token.Token);
+
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Logout()
+        {
+            RemoverCookieAutenticacao();
+
+            return NoContent();
+        }
+
+        private void DefinirCookieAutenticacao(string token)
+        {
+            Response.Cookies.Append(AuthCookieName, token, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTimeOffset.UtcNow.AddHours(1)
+            });
+        }
+
+        private void RemoverCookieAutenticacao()
+        {
+            Response.Cookies.Delete(AuthCookieName, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict
+            });
         }
     }
 }
